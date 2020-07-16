ad3b3d2f3b25fca7d35936b40931eef1c36741b4
sarahg813 <goon.sarah@gmail.com>
2020-07-15 23:41:54 -0400

    fixed styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,12 +2,11 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <p className="about">Track your world travels!</p>
-      <p className="about">Track your world travels!</p>
-      <p className="about">Track your world travels!</p>
-      <p className="about">Track your world travels!</p>
-      <p className="about">Track your world travels!</p>
+    <div className="about-container">
+      <p>Track your world travels!</p>
+      <p>Add a travel that you wanna remember.</p>
+      <p>Edit a travel if you've made a mistake.</p>
+      <p>Delete a travel if you wanna forget about it.</p>
     </div>
   );
 };
