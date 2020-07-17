ad7f12852883512408bd749f5ed2d33ad112cee0
sarahg813 <goon.sarah@gmail.com>
2020-07-17 01:01:48 -0400

    added build

---
+++ build/precache-manifest.0e3890be8300e6da542fca13977c603c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f7cf8ac5deb5a3f03f0f02bf2080ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afa31cb271a6b149834d",
+    "url": "/static/css/main.69c1521c.chunk.css"
+  },
+  {
+    "revision": "86ce878e35638c55a363",
+    "url": "/static/js/2.00168d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00168d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa31cb271a6b149834d",
+    "url": "/static/js/main.fc63b08c.chunk.js"
+  },
+  {
+    "revision": "5a6840c8d31c06a685ce",
+    "url": "/static/js/runtime-main.5ae3cf5a.js"
+  }
+]);
